8b37b2add9f57962c78ab1e427859a38145881c0
Neiver <neiver@localhost.localdomain>
2021-07-01 22:25:47 -0400

    Site livro v.2.0

---
+++ index.php
@@ -0,0 +1,226 @@
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Page Title</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"> 
+    
+    <!-- Title: Fonte para Giselle Azevedo -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet"> 
+
+<style>
+    
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        background-color: #f2f2f2;
+    }
+
+    h1,h2,h3,h4,h5,h6{
+        text-decoration: none;
+        font-weight: normal;
+    }
+
+    img{
+        width: 500;
+        float: left;
+        margin-right: 10px;
+    }
+
+    .btn-p{
+        background-color: #ff6600;
+        color: #fff;
+        padding: 10px;
+        margin-bottom: 20px;
+        border: none;
+        border-radius: 5px;
+        width: 100%;
+    }
+
+    .btn-p:hover{
+        background-color: #009999;
+    }
+
+    p{
+        margin-bottom: 30px;
+        font-size: 12pt;
+    }
+
+    .title{
+        font-family: 'Architects Daughter', cursive;
+        color: #fff;
+        padding: 50px 0 0 10px;
+    }
+    
+    a:link{
+        text-decoration: none;
+    }
+
+    .l-menu{
+        width: 200px;
+        height: 100%;
+        display: block;
+        position: fixed;
+        float: left;
+        padding: 0;
+        background-color: #ff6600;
+    }
+
+    .menu ul{
+        padding: 10px;
+        margin-top: 10%;
+    }
+
+    .menu ul li{
+        display: block;
+        padding: 10px;
+    }
+
+    .menu ul li a{
+        font-family: 'Poppins', sans-serif;
+        font-size: 20px;
+        color: #fff;
+    }
+
+    .menu ul li a:hover{
+        color: #000;
+    }
+    
+    /* --Texto publicitário do livro-- */
+    .r-content-img{
+        margin-left: 200px;
+        padding: 15px;
+        background-color: #f2f2f2;
+        font-family: 'Poppins', sans-serif;
+        font-size: 15pt;
+        width: 300px;
+        float: left;
+    }
+
+    /* --Área de texto-- */
+    .r-content-text{
+        padding: 15px;
+        background-color: #f2f2f2;
+        font-family: 'Poppins', sans-serif;
+        font-size: 15pt;
+        width: 500px;
+        float: left;
+    }
+
+    .r-content-p{
+        font-family: 'Architects Daughter', cursive;
+        padding: 15px;
+        margin: 10px;
+        background-color: #f2f2f2;
+        font-family: 'Poppins', sans-serif;
+        font-size: 15pt;
+        float: left;
+        border: 1px solid gray;
+        width: 200px;
+    }
+    
+    .footer{
+        clear: both;
+        background-color: #009999;
+        display: block;
+        width: 100%;
+        height: 150px;
+        padding: 0;
+        margin: 0;
+        bottom: 0;
+        position: relative;
+    }
+
+    .footer-link{
+        font-family: 'Architects Daughter', cursive;
+        font-size: 15pt;
+        color: #fff;
+        text-align: center;
+    }
+
+    /* ---- @media ----- */
+    @media only screen and (max-width: 600px) 
+    {
+
+        
+    }
+</style>
+
+</head>
+<body>
+
+    <div class="l-menu">
+    
+    <h1 class="title">Em defesa da Educação Pública</h1>
+
+        <!-- Menu lateral esquerdo -->
+        <div class="menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </div>
+    
+    </div>
+
+    <div class="r-content-img">
+    
+        <figure>
+            <img src=img/book_s.jpg alt="Figure">
+        </figure>
+    
+    </div>
+
+    <div class="r-content-text">
+        
+        <h3><b>Gestão Democrática e Projeto Político Pedagógico:</b> entre a ilusão e a realidade no contidiano de uma escola pública</h3>
+        
+        <p>Giselle Amaral</p>
+
+        <p><b>Sinópse</b></p>
+
+        <p>Esta obra, Gestão democrática e Projeto Político Pedagógico: entre a ilusão e a realidade no cotidiano de uma escola pública, lança um olhar sobre os estudos e as análises a respeito da gestão democrática, presente nos documentos legais e nos discursos oficiais de uma instituição de ensino pública, tão defendida no papel, mas pouco concretizada na escola. Essas diversas incongruências são desvendadas com base numa pesquisa realizada com a comunidade escolar, evidenciando as contradições entre o discurso oficial e as práticas vivenciadas pelos sujeitos da escola. É importante destacar que a consolidação de uma gestão democrática ainda é uma luta que se faz presente nas escolas públicas brasileiras. De maneira clara e objetiva, a autora faz uma retrospectiva histórica a respeito dos estudos da administração escolar no país, da gestão da educação brasileira, seus princípios e suas concepções, do percurso entre a adoção da concepção da administração e da gestão democrática com base nos documentos legais. Ainda, aborda interfaces entre o gerencialismo e a gestão democrática, bem como discute os modelos de planejamento e organização do trabalho pedagógico, visando trazer uma reflexão acerca do seguinte questionamento: “Projeto Político Pedagógico: uma construção coletiva?”.</p>
+
+        <p><b>Sobre a autora</b></p>
+
+        <p>Mais do que sistematizar e disponibilizar uma obra na área da Educação, espera-se suscitar questionamentos, inquietações, romper bloqueios e apontar caminhos, sem a intenção de esgotar as discussões acerca dessa temática, pois no domínio científico, toda conclusão é provisória, tendo em vista que a realidade é dinâmica, sujeita a mudanças, alterações. Logo, nesta obra, propõe-se levantar questões, em direção a uma prática pedagógica mais democrática, comprometida com a transformação social, buscando ouvir os sujeitos, seus posicionamentos e anseios, indo além do que está explicitado nos documentos oficiais. Esse é o maior desafio.</p>
+
+    </div>
+    
+    <div class="r-content-p">
+
+        <div style="text-align:center; margin-bottom: 10px;">
+
+            <h6>Capa Comum</h6>
+            
+            <h4><b>R$ 62,00</b></h4>
+        
+        </div>
+
+        <a href="https://www.editoraappris.com.br/produto/5281-gesto-democrtica-e-projeto-poltico-pedaggico-entre-a-iluso-e-a-realidade-no-cotidiano-de-uma-escola-pblica"><button class="btn-p">Comprar</button></a>
+        <h6>
+            <b>Detalhes:</b><br>
+            Editora ‏ : ‎ Appris; 1ª edição (23 maio 2021)<br>
+            Idioma ‏ : ‎ Português <br>
+            Capa comum ‏ : ‎ 193 páginas<br>
+            ISBN-10 ‏ : ‎ 6525005086<br>
+            ISBN-13 ‏ : ‎ 978-6525005089<br>
+            Dimensões ‏ : ‎ 16 x 2 x 23 cm
+        </h6>
+    </div>
+
+    <div class="footer">   
+        <p class="footer-link">Rodapé do site</p>
+    </div>
+        
+</body>
+</html> 
